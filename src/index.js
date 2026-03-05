ac1d6011e146295732526eea923dab49f83cd3ba
Varun <varunkothapally18@gmail.com>
2026-03-05 11:52:08 -0600

    Updated NetflixIndex component  inside render method

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {LoginComponent} from './components/login/login.component';
+import {NetflixIndex} from './netflix/netflix-index/netflix-index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginComponent />
+    <NetflixIndex />
   </React.StrictMode>
 );
 
