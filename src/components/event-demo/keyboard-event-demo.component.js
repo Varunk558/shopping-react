34f98c19baa824522372cc04fd986c48bdfb0919
Varun <varunkothapally18@gmail.com>
2026-03-23 11:15:33 -0500

    Added verify password feature

--- src/components/event-demo/keyboard-event-demo.component.js
+++ src/components/event-demo/keyboard-event-demo.component.js
@@ -5,6 +5,7 @@ export function KeyboardEventDemoComponent() {
     const [ users ] = useState([{"UserId": "john"},{"UserId": "jane"},{"UserId": "alice"},{"UserId": "bob"}]);
     const [ msg, setMsg ] = useState("");
     const [ errorClass, setErrorClass ] = useState("");
+    const [ toggle, setToggle ] = useState({ display: "none" });
 
     function verifyUserId(e) {
         for(let user of users) {
@@ -20,6 +21,15 @@ export function KeyboardEventDemoComponent() {
         }
         
     }
+
+    function verifyPassword(e) {
+        if(e.which >= 65 && e.which <= 90 ) {
+            setToggle({ display: "block" });
+        }else {
+            setToggle({ display: "none" });
+        }
+    }
+
     return (
         <div className="container-fluid">
             <h2>Keyboard Event Demo</h2>
@@ -28,7 +38,10 @@ export function KeyboardEventDemoComponent() {
                 <dd><input type="text" onKeyUp={verifyUserId}/></dd>
                 <dd className={errorClass}>{msg}</dd>
                 <dt>Password</dt>
-                <dd><input type="password" /></dd>
+                <dd><input type="password" onKeyPress={verifyPassword} /></dd>
+                <dd  style = {toggle} className="text-warning">
+                    <span className="bi bi-exclamation-triangle">Warning: Caps Lock is on</span>
+                </dd>
             </dl>
         </div>
     )
