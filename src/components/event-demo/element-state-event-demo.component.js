dd1afa1c8afd9bfb355d8538e33a254ccc08d746
Varun <varunkothapally18@gmail.com>
2026-03-24 10:19:01 -0500

    Updated with element state events

--- src/components/event-demo/element-state-event-demo.component.js
+++ src/components/event-demo/element-state-event-demo.component.js
@@ -6,6 +6,8 @@ export function ElementStateEventDemoComponent() {
 
     const [userName, setUserName] = useState("");
     const [userError, setUserError] = useState("");
+    const [city, setCity] = useState("");
+    const [cityError, setCityError] = useState("");
 
     function handleUserName(event) {
         setUserName(event.target.value);
@@ -16,10 +18,14 @@ export function ElementStateEventDemoComponent() {
             setUserError("User Name is required");
         } else {
             setUserError("");
-            document.write(`Hello !: ${userName}`);
+            document.write(`Hello ! ${userName} and your city is ${city}`);
         }
-    }
 
+        if (city == "notcity") {
+            setCityError("City is required");
+        }
+
+    }
     function HandleUserBlur(event) {
         if (event.target.value == "") {
             setUserError("User Name is required");
@@ -28,14 +34,48 @@ export function ElementStateEventDemoComponent() {
         }
     }
 
+    function HandleUserKeyUp(event) {
+        if (event.target.value == "") {
+            setUserError("User Name is required");
+        } else {
+            setUserError("");
+        }
+    }
+
+    function CityChanged(event) {
+        if (event.target.value == "notcity") {
+            setCityError("City is required");
+        } else {
+            setCity(event.target.value);
+            setCityError("");
+        }
+    }
+
+    function HandleUserFocus(event) {
+        setUserError("Name is Mandatory");
+    }
+
     return (
 
         <div className="container-fluid">
             <h2>Register User</h2>
             <dl>
                 <dt>User Name</dt>
-                <dd><input type="text" onBlur={HandleUserBlur} onChange={handleUserName} /></dd>
+                <dd><input type="text" onFocus={HandleUserFocus} onKeyUp={HandleUserKeyUp} onBlur={HandleUserBlur} onChange={handleUserName} /></dd>
                 <dd className="text-danger">{userError}</dd>
+                <dt>Your City</dt>
+                <dd>
+                    <select onChange={CityChanged}>
+                        <option value="notcity">Select City</option>
+                        <option value="Delhi">Delhi</option>
+                        <option value="Mumbai">Mumbai</option>
+                        <option value="Chennai">Chennai</option>
+                        <option value="Kolkata">Kolkata</option>
+                        <option value="Bangalore">Bangalore</option>
+                        <option value="Hyderabad">Hyderabad</option>
+                    </select>
+                </dd>
+                <dd className="text-danger">{cityError}</dd>
             </dl>
             <button onClick={SubmitClick} className="btn btn-primary">Submit</button>
         </div>
