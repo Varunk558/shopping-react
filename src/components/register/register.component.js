139c05c8d2c4f0e1fdc619b23da707976224c47c
Varun <varunkothapally18@gmail.com>
2026-03-28 11:13:30 -0500

    Added a RegisterComponent explaining how class component works

---
+++ src/components/register/register.component.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class RegisterComponent extends React.Component {
+
+
+    render(){
+        return(
+
+            <div className="container-fluid">
+                <h2>Register Component</h2>
+            </div>
+
+        )
+    }
+}
