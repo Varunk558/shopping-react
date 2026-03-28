6c8ba21ea5063837f1012db3fdb13680527ebddf
Varun <varunkothapally18@gmail.com>
2026-03-28 13:06:57 -0500

    Updated with using state in  a class component

--- src/components/register/register.component.js
+++ src/components/register/register.component.js
@@ -2,12 +2,46 @@ import React from "react";
 
 export class RegisterComponent extends React.Component {
 
+    constructor(){
+        super();
+        this.state ={
+            title: 'Product Details',
+            product: {
+                Name: "TV",
+                Price: "45000.56",
+                Stock: true
+            },
+            categories: ["All","Footwear", "Electronics", "Fashion"],
+            effect: 'text-success'
+        }
+    }
+
+    handleCategoryChange(e){
+        alert(e.target.value);
+    }
+    
 
     render(){
         return(
 
             <div className="container-fluid">
-                <h2>Register Component</h2>
+                <h2>{this.state.title}</h2>
+                <dl>
+                    <dt className={this.state.effect}>Name</dt>
+                    <dd>{this.state.product.Name}</dd>
+                    <dt>Price</dt>
+                    <dd>{this.state.product.Price}</dd>
+                    <dt>Stock</dt>
+                    <dd>{(this.state.product.Stock == true)?"Available":"Out Of Stock"}</dd>
+                </dl>
+                <h3>Select Category</h3>
+                <select onChange={this.handleCategoryChange}>
+                    {
+                        this.state.categories.map(category =>
+                            <option key={category}>{category}</option>
+                        )
+                    }
+                </select>
             </div>
 
         )
