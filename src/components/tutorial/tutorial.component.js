edd7b59d5e9d3923a223fb7385a6290f8f02850f
Varun <varunkothapally18@gmail.com>
2026-04-08 10:10:58 -0500

    Updated with using Link component to the route

--- src/components/tutorial/tutorial.component.js
+++ src/components/tutorial/tutorial.component.js
@@ -1,15 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export function TutorialComponent() {
   return (
       <div className="container-fluid">
         <h2>Tutorial - Web Technologies</h2>
         <BrowserRouter>
-          <Routes>
-            <Route path="html" element={<div><h3>HTML</h3><p>It is a markup language for creating web pages.</p></div>} />
-            <Route path="css" element={<div><h3>CSS</h3><p>It is a style sheet language used for describing the presentation of a document written in HTML.</p></div>} />
-            <Route path="javascript" element={<div><h3>JavaScript</h3><p>It is a programming language that enables interactive web pages.</p></div>} />
-          </Routes>
+            <section className="row">
+                <nav className="col-3">
+                    <div className="btn-group-vertical">
+                        <Link to="html" className="btn btn-outline-primary">HTML</Link>
+                        <Link to="css" className="btn btn-outline-primary">CSS</Link>
+                        <Link to="javascript" className="btn btn-outline-primary">JavaScript</Link>
+                    </div>
+
+                </nav>
+                <div className="col-9">
+                     <Routes>
+                        <Route path="html" element={<div><h3>HTML</h3><p>It is a markup language for creating web pages.</p></div>} />
+                        <Route path="css" element={<div><h3>CSS</h3><p>It is a style sheet language used for describing the presentation of a document written in HTML.</p></div>} />
+                         <Route path="javascript" element={<div><h3>JavaScript</h3><p>It is a programming language that enables interactive web pages.</p></div>} />
+                     </Routes>
+                </div>
+            </section>
         </BrowserRouter>
       </div>   
   );
